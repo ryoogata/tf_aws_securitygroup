fa17b5457b945d6fe191b081b25d7b2b3477f7a5
ryoogata <ryo.ogata@gmail.com>
2015-09-25 17:49:55 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+# security group の作成
+resource "aws_security_group" "default_sg" {
+  name = "default_sg"
+  description = "Default SG"
+  vpc_id="${var.vpc_id}"
+
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = -1
+      to_port = -1
+      protocol = "icmp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.tag}"
+  }
+}
